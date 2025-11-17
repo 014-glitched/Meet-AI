09b76733d7a0eca1cc17f11c6dcf8a1675b6876b
Abhyu Srivastava <abhyu0011@gmail.com>
2025-11-17 13:59:20 +0530

    created Auth pages, auth layout, auth module, sign-in and  sign-up form

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import { SignInView } from '@/src/modules/auth/ui/views/sign-in-view'
+
+const page = () => {
+  return (
+    <SignInView />
+  )
+}
+
+export default page
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import { Card } from '@/src/components/ui/card'
+import { SignUpView } from '@/src/modules/auth/ui/views/sign-up-view'
+import React from 'react'
+
+const page = () => {
+  return (
+    <SignUpView />
+  )
+}
+
+export default page
