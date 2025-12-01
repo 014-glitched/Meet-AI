f3e7247ee069b570aafafb80576812390ed56fd3
Abhyu Srivastava <abhyu0011@gmail.com>
2025-12-02 02:04:38 +0530

    added responsive dialog && changes in global.css

--- src/modules/dashboard/ui/components/dashboard-command.tsx
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -1,4 +1,4 @@
-import { CommandDialog, CommandInput, CommandItem, CommandList } from '@/src/components/ui/command'
+import { CommandResponsiveDialog, CommandInput, CommandItem, CommandList } from '@/src/components/ui/command'
 import React, { Dispatch, SetStateAction } from 'react'
 
 interface Props {
@@ -8,7 +8,7 @@ interface Props {
 
 const DashboardCommand = ({ open, setOpen }: Props ) => {
   return (
-    <CommandDialog open={open} onOpenChange={setOpen} >
+    <CommandResponsiveDialog open={open} onOpenChange={setOpen} >
         <CommandInput 
             placeholder='Find a meeting or agent'
         />
@@ -17,7 +17,7 @@ const DashboardCommand = ({ open, setOpen }: Props ) => {
                 Test
             </CommandItem>
         </CommandList>
-    </CommandDialog>
+    </CommandResponsiveDialog>
   )
 }
 
