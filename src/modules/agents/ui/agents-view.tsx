eec164a6342a825cda389ccde3a668a3c86208f8
Abhyu Srivastava <abhyu0011@gmail.com>
2025-12-09 14:47:26 +0530

    added data table shadcn component & a empty state component

--- src/modules/agents/ui/agents-view.tsx
+++ src/modules/agents/ui/agents-view.tsx
@@ -4,14 +4,25 @@ import ErrorState from "@/src/components/error-state";
 import LoadingState from "@/src/components/loading-state";
 import {useTRPC} from "@/src/trpc/client";
 import {useSuspenseQuery} from "@tanstack/react-query";
+import { DataTable } from "./data-table";
+import { columns } from "./column";
+import EmptyState from "@/src/components/empty-state";
+
 
 export const AgentsView = () => {
   const trpc = useTRPC();
   const {data} = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
   return( 
-    <div>
-        {JSON.stringify(data, null, 2)}
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+        {/* {JSON.stringify(data, null, 2)} */}
+        <DataTable data={data} columns={columns}/>
+        {data.length === 0 && (
+          <EmptyState 
+            title="Create your first agent"
+            descripton="Create an agent to join meetings. Each agent will follow your instruction and can interact with participants during the call."
+          />
+        )}
     </div>
 )};
 
